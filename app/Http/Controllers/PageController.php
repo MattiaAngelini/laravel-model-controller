c13e3d225bc3d8def24b1e50d3b3f169227f9563
MattiaAngelini <mattiaangelini1993@gmail.com>
2024-05-21 23:42:54 +0200

    feat: printed movie cards

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -9,7 +9,7 @@ class PageController extends Controller
 {
     public function index() {
         $movies = Movie::all();
-        dd($movies);
-        return view('home');
+     
+        return view('home', ['movies' => $movies]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-    
+
+@foreach ($movies as $movie)
+
+
+<div class="card" style="width: 30rem;">
+    <div class="card-body">
+      <h5 >Titolo: {{$movie -> title}}</h5>
+      <h5>Titolo Originale: {{$movie -> original_title}}</h5>
+      <h5>Nazionalità: {{$movie -> nationality}}</h5>
+      <h5>Data uscita: {{$movie -> date}}</h5>   
+      <h5>Voto: {{$movie -> vote}}</h5>
+    </div>
+</div>
+
+
+@endforeach
+
 @endsection
